f994a4f1d642b091a99e643f03b3177a566f96bb
Omar ABASSI <oabassi@assurone-group.com>
2020-01-03 17:57:28 +0100

    change version of angular

--- AppAngular/src/app/app.component.spec.ts
+++ AppAngular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ApplicationAngular'`, () => {
+  it(`should have as title 'AppAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ApplicationAngular');
+    expect(app.title).toEqual('AppAngular');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ApplicationAngular!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AppAngular app is running!');
   });
 });
