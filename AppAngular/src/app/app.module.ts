3a94f6d6c210d7543e024bcdd11d4304bf108df9
Omar ABASSI <oabassi@assurone-group.com>
2020-01-06 16:56:50 +0100

    add layout header body footer

--- AppAngular/src/app/app.module.ts
+++ AppAngular/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
